4c0ba35df7e78bc6e7f894ac1b19799a3115c276
Donát Fehér <50624191+Dodni@users.noreply.github.com>
2021-11-30 16:17:47 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamWriter writetext = new StreamWriter("write.txt"))
+            {
+                writetext.WriteLine("");
+            }
+            using (StreamReader readtext = new StreamReader("write.txt"))
+            {
+                string readText = readtext.ReadLine();
+                Console.WriteLine(readText);
+            }
+            //string asd;
+            var asd = new StringBuilder();
+            int sampleRate = 8000;
+            short[] buffer = new short[8000];
+            double amplitude = 0.25 * short.MaxValue;
+            double frequency = 1000;
+            for (int n = 0; n < buffer.Length; n++)
+            {
+                buffer[n] = (short)(amplitude * Math.Sin((2 * Math.PI * n * frequency) / sampleRate));
+                asd.Append(" " + buffer[n].ToString() + "\n");
+                
+                
+            }
+
+            using (StreamWriter writetext = new StreamWriter("write4.csv"))
+            {
+                writetext.WriteLine(asd);
+            }
+        }
+    }
+}
